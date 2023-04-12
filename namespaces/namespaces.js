c61a749d6e30bde3011a63820ecb3fcc22d648f0
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-04-12 15:02:05 +0900

    feat: namespace 분리 및 이벤트리스너 설정

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -5,9 +5,19 @@ app.use(express.static(__dirname + '/public'));
 const expressServer = app.listen(8001);
 const io = socketio(expressServer);
 
-io.on('connection', (socket) => {  
+
+// default namespace = .of("/")
+io.on('connection', (socket) => {
+
+  // mainspace에 유저가 접속했음을 admin namespace에 전달할 수 있다.
+  io.of("/admin").emit("userJoinedMainNs", () => {console.log(socket.id,"has joined main namespace.")})
   socket.on('newMessageToServer',(dataFromClient) => {
     console.log('Data:',dataFromClient);
     io.emit('newMessageToClients', { text: dataFromClient.text })
   })
+})
+
+io.of('/admin').on('connection', (socket) => {
+  console.log(socket.id, "has joinded /admin namespaces");
+  io.of("admin").emit('messageToClientsFromAdmin',() => {});
 })
